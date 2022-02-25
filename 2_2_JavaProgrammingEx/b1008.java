4a7bc717b0325a80a96ef2ac34ab82b4804cea9a
dubidubob <87163850+dubidubob@users.noreply.github.com>
2022-02-25 21:16:09 +0900

    Create b1008.java

---
+++ 2_2_JavaProgrammingEx/b1008.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] arg) {
+	Scanner scanner =new Scanner(System.in);
+	System.out.print(scanner.nextDouble()/scanner.nextInt());
+	scanner.close();
+	}
+}
