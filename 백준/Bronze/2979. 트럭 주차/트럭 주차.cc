249645234007ed5f4cff24cad6a3e1fac6ee9c64
Soyeon Joo <87163850+dubidubob@users.noreply.github.com>
2024-05-12 01:33:49 +0900

    Update 트럭 주차.cc

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -27,3 +27,51 @@ int main()
 	
 	cout << sum;
 }
+
+/* 이거 왜 안 될까...
+#include <iostream>
+#include <vector>
+			using namespace std;
+
+		int main()
+		{
+			//초기화 어떻게 하는지
+			vector<int> carpark(105, 0);
+
+			//각 차가 들어오고 나가는 시간
+			int i, j;
+			//차 1대 2대 3대 별 주차값
+			int coin1, coin2, coin3;
+			//1대, 2대, 3대일 때 총 시간
+			int count1 = 0, count2 = 0, count3 = 0;
+
+			cin >> coin1 >> coin2 >> coin3;
+
+			for (int l = 0; l < 3; l++)
+			{
+				cin >> i >> j;
+
+				for (int k = i; k < j; k++)
+				{
+					carpark[k]++;
+				}
+			}
+
+			//carpark.resize(j);
+
+			int sum = 0;
+			//if문 3개 이하면 if > switch
+			for (int i = 0; i < j; i++)
+			{
+				if (carpark[i] == 1)
+					sum += carpark[i] * coin1;
+				else if (carpark[i] == 2)
+					sum += carpark[i] * coin2;
+				else if (carpark[i] == 3)
+					sum += carpark[i] * coin3;
+			}
+			cout << sum;
+
+			return 0;
+		}
+*/
