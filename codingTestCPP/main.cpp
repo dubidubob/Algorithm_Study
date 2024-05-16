5f16d03c657909461a4cebf6140281ca46f7883f
dubidubob <2076397@ewhain.net>
2024-05-17 02:31:51 +0900

    bj

--- codingTestCPP/main.cpp
+++ codingTestCPP/main.cpp
@@ -1,46 +1,56 @@
-/*
-N 노드 중 노드 5개가 연결되어 있는 유니온이 존재하는지 찾는 문제
-
-노드 수 N(2000)
-엣지 수 M(2000)
-
-1. 인접리스트로 관계 전부 삽입
-2. DFS 실행
-	각 노드 타고 들어간 뒤, 다음 노드로 전환
-	중요. count 함수로 관계 1 깊어질 때마다 업데이트 
-		count == 5 일 시 1 출력 후 return
-		아닐 시 continue, 마지막에 0 출력 후 return
-
-인접리스트 구현 방법!
-
-*/
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void main()
+void DFS(vector<vector<int>> A, vector<bool> visited, int start);
+void BFS(vector<vector<int>> A, vector<bool> visited, int start);
+int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	//인접리스트 구현하는 법!
-	vector<vector <int>> friends;
+	vector<vector<int>> A;
 	vector<bool> visited;
-	int N; //친구, 노드 개수
-	int M; //친구 관계, 엣지 개수
+	int N, M, start;
+	cin >> N >> M >> start;
 
-	cin >> N >> M;
-
-	friends.resize(N + 1);
-	visited.resize(N + 1);
-	//인접리스트로 관계 받기
+	A.resize(N + 1);
+	
+	//인접리스트에 모두 저장
 	for (int i = 0; i < M; i++)
 	{
-		int start, end;
-		cin >> start >> end;
-		friends[start].push_back(end);
-		friends[end].push_back(start);
+		int s, e;
+		cin >> s >> e;
+		A[s].push_back(e);
+		A[e].push_back(s);
 	}
+
+	visited.resize(N + 1, false);
+	DFS(A, visited, start);
+	visited.resize(N + 1, false);
+	BFS(A, visited, start);
+}
+
+void DFS(vector<vector<int>> A, vector<bool> visited, int start)
+{
+	if (visited[start])
+	{
+		return;
+	}
+
+	visited[start] = true;
+	cout << start<<" ";
+	
+	for (int i : A[start])
+	{
+		if (!visited[i])
+		{
+			DFS(A, visited, i);
+		}
+	}
+}
+
+void BFS(vector<vector<int>> A, vector<bool> visited, int start)
+{
 }
