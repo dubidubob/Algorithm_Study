84fb3134c5d38e7765d003e0d19dff133731c240
Soyeon Joo <87163850+dubidubob@users.noreply.github.com>
2025-05-08 18:58:09 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,83 +1,70 @@
-/*
-대체 왜!!!! 다 줬잖아!! 다 줬잖아!!!!!!!!!
-
-1. #include <algorithm> //sort를 가지려면 필요
-2.
-vector<bool> visited;
-vector<vector<int>> graph;
-이거 static 선언하든가 & 참조 형식으로 바꾸기! 아니면 복사된다~!
-3. fill(visited.begin(), visited.end(), false); //방문 배열 초기화
-4. while (!BFSqueue.empty()){ //while문 추가!!!
-*/
-
 #include <iostream>
-#include <vector>
-#include <algorithm> //sort를 가지려면 필요
 #include <queue>
+#include <algorithm>
 using namespace std;
 
-vector<bool> visited;
 vector<vector<int>> graph;
-int node, edge, start;
+vector<bool> visited;
 
-void DFS(int node);
-void BFS(int node);
+void dfs(int cur)
+{
+	cout << cur << " ";
+	visited[cur] = true;
+
+	for (int child : graph[cur])
+	{
+		if (!visited[child])
+		{
+			dfs(child);
+		}
+	}
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	
+	int v, e, s;
+	cin >> v >> e >> s;
 
-	cin >> node >> edge >> start;
-	visited.resize(node + 1, false);
-	graph.resize(node + 1);
+	graph.resize(v+1);
+	for (int i = 0; i < e; i++)
+	{
+		int f, l;
+		cin >> f >> l;
 
-	for (int i = 0; i < edge; i++) {
-		int start, end;
-		cin >> start >> end;
-		graph[start].push_back(end);
-		graph[end].push_back(start);
+		graph[f].push_back(l);
+		graph[l].push_back(f);
 	}
 
-	for (int i = 1; i <= node; i++) {
-		sort(graph[i].begin(), graph[i].end());//작은 순으로 뱉어라~!
+	for (int i = 1; i <= v; i++)
+	{
+		sort(graph[i].begin(), graph[i].end());
 	}
 
-	DFS(start);
+	visited.resize(v+1, false);
+	dfs(s);
+
+	visited.clear();
+	visited.resize(v+1, false);
 	cout << "\n";
-	fill(visited.begin(), visited.end(), false); //방문 배열 초기화
-	BFS(start);
+
+	queue<int> q;
+	q.push(s);
+	while (!q.empty())
+	{
+		int cur = q.front(); q.pop();
+		if (visited[cur])
+			continue;
+		visited[cur] = true;
+		cout << cur <<" ";
+
+		for (int child : graph[cur])
+		{
+			if (visited[child])
+				continue;
+			q.push(child);
+		}
+	}
 
 	return 0;
-}
-
-void DFS(int node) {
-	cout << node << " ";
-
-	visited[node] = true;
-	//cout << "\nvisited[" << node << "] = " << (bool)visited[node];
-	for (int i : graph[node]) {
-		if (!visited[i]) {
-			DFS(i);
-		}
-	}
-}
-
-void BFS(int node) {
-	queue <int> BFSqueue;
-	BFSqueue.push(node);
-	visited[node] = true;
-
-	while (!BFSqueue.empty()) { //while문 추가!!!
-		int now = BFSqueue.front();
-		BFSqueue.pop();
-		cout << now << " ";
-
-		for (int i : graph[now]) {
-			if (!visited[i]) {
-				visited[i] = true;
-				BFSqueue.push(i);
-			}
-		}
-	}
 }
