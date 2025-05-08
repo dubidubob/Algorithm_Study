014ac017dbfff0528733028ea90ca3c23a6aa498
Soyeon Joo <87163850+dubidubob@users.noreply.github.com>
2025-05-08 14:59:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,23 +1,13 @@
 /*
-0. iterator로 해보기 <-> sorting을 통한 index 직접 지정
-1. test case 
-10 5 1
-1 10
-10 5
-10 4
-10 3
-10 2
-for (int i = 1; i <= node; i++)
-<->
-for (int i = 0; i < node; i++)
-생각해보기
-2. #include <algorithm> //sort를 가지려면 필요
-3.
+대체 왜!!!! 다 줬잖아!! 다 줬잖아!!!!!!!!!
+
+1. #include <algorithm> //sort를 가지려면 필요
+2.
 vector<bool> visited;
 vector<vector<int>> graph;
 이거 static 선언하든가 & 참조 형식으로 바꾸기! 아니면 복사된다~!
-4. fill(visited.begin(), visited.end(), false); //방문 배열 초기화
-5. while (!BFSqueue.empty()){ //while문 추가!!!
+3. fill(visited.begin(), visited.end(), false); //방문 배열 초기화
+4. while (!BFSqueue.empty()){ //while문 추가!!!
 */
 
 #include <iostream>
