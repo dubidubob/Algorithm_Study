1064ebac83d0a31dbce34fc119e6a689ef9887e6
Soyeon Joo <87163850+dubidubob@users.noreply.github.com>
2025-05-27 02:24:41 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-long long mod_pow(long long base, long long exp, long long mod) {
-    if (exp == 0) return 1;
-    if (exp == 1) return base % mod;
+long long a, b, c;
 
-    long long result = mod_pow(base, exp / 2, mod);
-    result = (result * result) % mod;
-
-    if (exp % 2 == 1)
-        result = (result * base) % mod;
+long long dp(long long level) {
+    if (level == 0) return 1;
+    if (level == 1)
+        return a % c;
+   
+    long long result = dp(level / 2);
+    result = (result*result) % c;
+    if (level % 2 == 1) {
+        result = (result * a) % c;
+    }
 
     return result;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-    long long a, b, c;
     cin >> a >> b >> c;
 
-    cout << mod_pow(a, b, c) << "\n";
-
+    cout << dp(b);
+    
     return 0;
 }
