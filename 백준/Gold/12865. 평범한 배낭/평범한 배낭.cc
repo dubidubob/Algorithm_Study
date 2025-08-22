1ab2b9e0c5e5247f4db06c76c18dcb50cc5dafc2
Soyeon Joo <87163850+dubidubob@users.noreply.github.com>
2025-08-22 20:05:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -4,40 +4,20 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	
+	int n, k;
+	cin >> n >> k;
 
-    int n, k;
-    cin >> n >> k;
-    
-    //각 아이템 정보
-    vector<pair<int, int>> items(1, {0, 0});
-    int w, v;
+	int dp[100001] = {0};
 
-		//핉터 1: 아이템 정보 저장
-    for (int i = 1; i <= n; i++) {
-        cin >> w >> v;
-        items.push_back({w, v});
-    }
+	for (int i = 1; i <= n; i++) {
+		int w, v;
+		cin >> w >> v;
+		for (int j = k; j >= w; j--) {
+			dp[j] = max(dp[j], dp[j - w] + v);
+		}
+	}
 
-    // 필터 2 : 무게에 따라 정렬, C++ sort!
-    sort(items.begin(), items.end());
-
-    // 필터 3 : 아이템 키마다 준서 무게 게이지가 차있다.
-    vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-
-    // 냅색 알고리즘
-    for (int i = 1; i <= n; i++) {// N개 아이템 순회
-        for (int j = 0; j <= k; j++) { // Kkg 물건 제한 
-            if (j - items[i].first >= 0) {// Wkg 물건 검사
-                dp[i][j] = max(dp[i - 1][j - items[i].first] + items[i].second, dp[i - 1][j]);// V 가치 비교
-            } else {//넘으면 bye
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
-    }
-
-    cout << dp[n][k] << "\n";
-
-    return 0;
+	cout << dp[k];
 }
